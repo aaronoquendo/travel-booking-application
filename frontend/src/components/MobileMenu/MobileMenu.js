0fbf6471decf0247bff663a727ec08088fd51b84
Aaron <iaaron94@gmail.com>
2019-04-21 08:48:15 -0400

    adding main.scss to mobilemenu portal

--- frontend/src/components/MobileMenu/MobileMenu.js
+++ frontend/src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/main.scss'
+import '../../assets/styles/main.scss'
 class MobileMenu extends Component {
   constructor(props) {
     super(props)
